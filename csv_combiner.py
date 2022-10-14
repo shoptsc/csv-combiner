fd6b8382cc59088b237f910efa11b6ef0328066b
sanusi hamed <sanusihamed@gmail.com>
2022-10-14 00:29:55 -0400

    comment added to the code

--- csv_combiner.py
+++ csv_combiner.py
@@ -3,18 +3,24 @@ import sys
 
 def csvCombiner(files):
 
+    # Create an empty dataframe.
     dataframe = pd.DataFrame()
+    
+    # Create an empty list to add the file names.
     fileName = []
     fileNameList = []
             
     for i in range(1, len(files)):
         data = pd.read_csv(files[i])
+        # append the file names to the empty list
         fileName.append(list(len(data) * (files[i].replace("./fixtures/", ""),)))
         dataframe  = pd.concat([dataframe, data])
 
+    # Spread the nested array into one single list.
     for i in fileName:
         fileNameList.extend(i)
 
+    # Add the file name as extra column with header filename and convert to csv.
     dataframe['filename'] = fileNameList
     return (dataframe.to_csv(index = False, lineterminator='\r'))
    
